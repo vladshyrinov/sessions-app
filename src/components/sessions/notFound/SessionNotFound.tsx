9eac30f9ed2e7fbf8f2741243cd58a68196cff2b
Vladyslav Shyrinov <vlad.shyrinov@gmail.com>
2025-04-13 23:18:22 +0200

    Removed unnecessary vertical scrolling for the pages

--- src/components/sessions/notFound/SessionNotFound.tsx
+++ src/components/sessions/notFound/SessionNotFound.tsx
@@ -1,6 +1,6 @@
 export default function SessionNotFound() {
   return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-300">
+    <main className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-300">
       <div className="container mx-auto p-6">
         <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-6">
           Session Not Found
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,7 +4,7 @@ import SessionsClearButton from "../components/sessions/removal/SessionsClearBut
 
 export default function DashboardPage() {
   return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-300">
+    <main className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-300">
       <div className="container mx-auto p-6">
         <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-6">
           Dashboard
